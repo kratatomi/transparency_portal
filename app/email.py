20331459b6538f161a22001e420f74f7890096d6
kratatomi <contact@kationickratom.com>
2022-01-01 12:50:48 +0100

    Fix mail notification on proposal submission.

--- app/email.py
+++ app/email.py
@@ -2,14 +2,15 @@ from app import app
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import mail_settings
 
 def send_new_proposal_email(proposal):
     from_addr = app.config['ADMINS'][0]
     to_addr = app.config['ADMINS'][0]
     text = f'Proposal ID {proposal.id} submitted'
 
-    username = app.config["MAIL_USERNAME"]
-    password = app.config["MAIL_PASSWORD"]
+    username = mail_settings.MAIL_USERNAME
+    password = mail_settings.MAIL_PASSWORD
 
     msg = MIMEMultipart()
 
@@ -19,7 +20,7 @@ def send_new_proposal_email(proposal):
     msg.attach(MIMEText(text))
 
 
-    server = smtplib.SMTP(app.config['MAIL_SERVER'], app.config['MAIL_PORT'])
+    server = smtplib.SMTP(mail_settings.MAIL_SERVER, mail_settings.MAIL_PORT)
     server.ehlo()
     server.starttls()
     server.ehlo()
