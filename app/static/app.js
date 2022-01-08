2186bce9ae8b7e4fdaf7719d39648a9b18a22b3f
kratatomi <contact@kationickratom.com>
2022-01-08 23:40:16 +0100

    Fix redirection upon login.

--- app/static/app.js
+++ app/static/app.js
@@ -1,5 +1,3 @@
-// import { ethers } from "https://cdn.ethers.io/lib/ethers-5.2.esm.min.js";
-
 let provider;
 let accounts;
 
@@ -9,97 +7,89 @@ let signer;
 function login() {
   console.log("oh hey there");
 
-  // signer.signMessage("hello");
-
   rightnow = (Date.now() / 1000).toFixed(0);
   sortanow = rightnow - (rightnow % 600);
 
   signer
     .signMessage(
-      "Signing in to transparency.smartindex.cash at " + sortanow,
-      accountAddress,
-      "test password!"
+      "Signing in to transparency.smartindex.cash at " + sortanow
     )
     .then((signature) => {
       handleAuth(accountAddress, signature);
     });
 }
 
-function handleAuth(accountAddress, signature) {
+async function handleAuth(accountAddress, signature) {
   console.log(accountAddress);
   console.log(signature);
 
-  fetch("login", {
+  const response = await fetch("login", {
     method: "post",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify([accountAddress, signature]),
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    });
+  });
+
+
+  if (response.ok && response.redirected) {
+       window.location.href = response.url;
+  }
 }
 
-ethereum.enable().then(function () {
-  provider = new ethers.providers.Web3Provider(web3.currentProvider);
+ethereum.request({method: 'eth_requestAccounts'}).then(async function (accounts) {
+  provider = new ethers.providers.Web3Provider(ethereum);
 
-  provider.getNetwork().then(function (result) {
-    if (result["chainId"] != 10000) {
-      document.getElementById("msg").textContent = "Switch to SmartBCH!";
-    } else {
-      // okay, confirmed we're on mainnet
+  const network = await provider.getNetwork();
+  if (network["chainId"] !== 10000) {
+    document.getElementById("msg").textContent = "Switch to SmartBCH!";
+    return;
+  }
 
-      provider.listAccounts().then(async function (result) {
-        console.log(result);
-        accountAddress = result[0]; // getting users publickey
+  // okay, confirmed we're on mainnet
+  console.log(accounts);
+  accountAddress = ethers.utils.getAddress(accounts[0]); // getting users publickey
 
-        // contract address and contract abi is used to create the contract instance
-        const contractAddress = "0xF05bD3d7709980f60CD5206BddFFA8553176dd29";
-        const contractABI = [
-          // balanceOf
-          {
-            constant: true,
-            inputs: [{ name: "_owner", type: "address" }],
-            name: "balanceOf",
-            outputs: [{ name: "balance", type: "uint256" }],
-            type: "function",
-          },
-          // decimals
-          {
-            constant: true,
-            inputs: [],
-            name: "decimals",
-            outputs: [{ name: "", type: "uint8" }],
-            type: "function",
-          },
-        ];
+  // contract address and contract abi is used to create the contract instance
+  const contractAddress = "0xF05bD3d7709980f60CD5206BddFFA8553176dd29";
+  const contractABI = [
+    // balanceOf
+    {
+      constant: true,
+      inputs: [{ name: "_owner", type: "address" }],
+      name: "balanceOf",
+      outputs: [{ name: "balance", type: "uint256" }],
+      type: "function",
+    },
+    // decimals
+    {
+      constant: true,
+      inputs: [],
+      name: "decimals",
+      outputs: [{ name: "", type: "uint8" }],
+      type: "function",
+    },
+  ];
 
-        // creating contract instance
-        const contract = new ethers.Contract(
-          contractAddress,
-          contractABI,
-          provider
-        );
+  // creating contract instance
+  const contract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    provider
+  );
 
-        // getting the contract decimals and balance
-        const decimals = await contract.decimals();
-        let balance = await contract.balanceOf(accountAddress);
-        balance = parseFloat(ethers.utils.formatUnits(balance, decimals));
+  // getting the contract decimals and balance
+  const decimals = await contract.decimals();
+  let balance = await contract.balanceOf(accountAddress);
+  balance = parseFloat(ethers.utils.formatUnits(balance, decimals));
 
-        if (balance < 5000) {
-          // if SIDX  balance < 5000, insufficient balance
-          document.getElementById("msg").textContent =
-            "You need at least 5000 SIDX tokens to submit a proposal";
-        } else {
-          // else allow user to submit proposal
-          const submitProposalButton =
-            document.getElementById("login");
-          submitProposalButton.disabled = false;
-          signer = provider.getSigner();
-        }
-      });
-    }
-  });
+  if (balance < 5000) {
+    // if SIDX  balance < 5000, insufficient balance
+    document.getElementById("msg").textContent =
+      "You need at least 5000 SIDX tokens to submit a proposal";
+  } else {
+    // else allow user to submit proposal
+    const submitProposalButton =
+      document.getElementById("login");
+    submitProposalButton.disabled = false;
+    signer = provider.getSigner();
+  }
 });
