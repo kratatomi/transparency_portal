1f88df3e933f4b34d4f4cb5e925567e9b0df92bd
kratatomi <contact@kationickratom.com>
2022-12-11 14:55:59 +0100

    Update SIDX liquidity pools after taking weekly yields.

--- engine.py
+++ engine.py
@@ -70,10 +70,10 @@ assets_balances = {
 }
 
 initial_pool_balances = {
-    "Mistswap": {"CA": "0x7E1B9F1e286160A80ab9B04D228C02583AeF90B5", "token0": 8.6711, "token1": 18232.47},
-    "BlockNG": {"CA": "0x1CD36D9dEd958366d17DfEdD91b5F8e682D7f914", "token0": 2865.52, "token1": 3765.05},
-    "Tangoswap": {"CA": "0x4509Ff66a56cB1b80a6184DB268AD9dFBB79DD53", "token0": 4.7755, "token1": 1797.38},
-    "Emberswap": {"CA": "0x97dEAeB1A9A762d97Ac565cD3Ff7629CD6d55D09", "token0": 254673, "token1": 705.32}
+    "Mistswap": {"CA": "0x7E1B9F1e286160A80ab9B04D228C02583AeF90B5", "token0": 8.6836, "token1": 18245.10},
+    "BlockNG": {"CA": "0x1CD36D9dEd958366d17DfEdD91b5F8e682D7f914", "token0": 2913.39, "token1": 3824.47},
+    "Tangoswap": {"CA": "0x4509Ff66a56cB1b80a6184DB268AD9dFBB79DD53", "token0": 4.7925, "token1": 1814.73},
+    "Emberswap": {"CA": "0x97dEAeB1A9A762d97Ac565cD3Ff7629CD6d55D09", "token0": 259263, "token1": 709.36}
     # Token0 is WBCH/LAW/EMBER, Token1 is SIDX
 }
 
