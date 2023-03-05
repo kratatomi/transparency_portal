abe9aaca0f9f68d0825de5c6364c8aed634c8295
kratatomi <contact@kationickratom.com>
2023-03-05 14:09:11 +0100

    Add a breakpoint to take_weekly_yields() functions if farms aren't harvested by an error.

--- yield_snapshot.py
+++ yield_snapshot.py
@@ -6,6 +6,7 @@ from os import listdir, getcwd
 from os.path import isfile, join, abspath
 from datetime import datetime
 import logging
+import sys
 
 # Globally set some graph params
 mpl.rcParams['text.color'] = '#f8fdff'
@@ -299,7 +300,7 @@ def take_weekly_yields(stacked_assets, farms):
         logger.error(f'Function harvest_farms_rewards failed. Exception: {e}')
         import app.email as email
         email.send_email_to_admin(f'Function harvest_farms_rewards failed. Exception: {e}')
-        break
+        sys.exit()
     else:
         # We have to take the profits from the BCH/bcBCH and flexUSD/BCH farms and swap them for bcUSDT (proposal #42)
         # With proposal #50, the only farm left is BCH/bcBCH.
