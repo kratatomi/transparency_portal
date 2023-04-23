07e012f9166efbbc33b6fd170c3051eec19dcb07
kratatomi <contact@kationickratom.com>
2023-04-23 15:58:57 +0200

    Update initial_pool_balances after taking weekly rewards.

--- engine.py
+++ engine.py
@@ -76,10 +76,10 @@ assets_balances = {
 }
 
 initial_pool_balances = {
-    "Mistswap": {"CA": "0x7E1B9F1e286160A80ab9B04D228C02583AeF90B5", "token0": 8.8471, "token1": 18475.47},
-    "BlockNG": {"CA": "0x1CD36D9dEd958366d17DfEdD91b5F8e682D7f914", "token0": 3545.26, "token1": 4595.76},
-    "Tangoswap": {"CA": "0x4509Ff66a56cB1b80a6184DB268AD9dFBB79DD53", "token0": 5.0607, "token1": 2184.21},
-    "Emberswap": {"CA": "0x97dEAeB1A9A762d97Ac565cD3Ff7629CD6d55D09", "token0": 421061, "token1": 884.57}
+    "Mistswap": {"CA": "0x7E1B9F1e286160A80ab9B04D228C02583AeF90B5", "token0": 8.8549, "token1": 18493.54},
+    "BlockNG": {"CA": "0x1CD36D9dEd958366d17DfEdD91b5F8e682D7f914", "token0": 3588.44, "token1": 4653.06},
+    "Tangoswap": {"CA": "0x4509Ff66a56cB1b80a6184DB268AD9dFBB79DD53", "token0": 5.0703, "token1": 2206.45},
+    "Emberswap": {"CA": "0x97dEAeB1A9A762d97Ac565cD3Ff7629CD6d55D09", "token0": 428087, "token1": 904.37}
     # Token0 is WBCH/LAW/EMBER, Token1 is SIDX
 }
 
